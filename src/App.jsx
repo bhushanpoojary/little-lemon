c2ac8ad477bd413c44ba52f2e16886b22b334d44
bhushanpoojary <64708832+bhushanpoojary@users.noreply.github.com>
2024-12-13 12:26:43 +0530

    Addded Header, Nav, Main and Footer components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css';
+import Header from "./Header";
+import Main from './Main';
+import Nav from "./Nav";
+import Footer from './Footer';
 
 function App() {
   return (
     <>
       <header>
+        <Header/>
       </header>
       <nav>
+        <Nav/>
       </nav>
       <main>
+        <Main/>
       </main>
       <footer>
+        <Footer/>
       </footer>
     </>
   );
